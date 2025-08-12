4f1668f04d8d3350e43b49a0593e714b3405181c
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-08-12 21:29:11 +0200

    add socket.io-client integration to AppComponent for real-time communication

--- chat-client/src/app/app.component.ts
+++ chat-client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,8 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    const socket = io('http://localhost:4000', { transports: ['websocket'] });
+    socket.on('hello', (msg) => console.log('SERVER SAGT:', msg));
+  }
 }
